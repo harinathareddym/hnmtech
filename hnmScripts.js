609d7e2da6b0d72034d5db2c70bb678ce9f5af37
harinathareddym <45412511+harinathareddym@users.noreply.github.com>
2025-02-10 10:31:36 +0530

    Delete hnmScripts.js

--- hnmScripts.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const text = document.querySelector('.animated-text');
-    text.innerHTML = text.textContent.split('').map(char => `<span>${char}</span>`).join('');
-});
